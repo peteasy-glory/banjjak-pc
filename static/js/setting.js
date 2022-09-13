b9712cd0605fe79cf45d6e1cdbf23f99403db7a6
glory <glory@peteasy.kr>
2022-09-13 18:53:25 +0900

    상품후기답글 이모지 있을때 오류 수정 일정관리 휴게시간 없을때 오류 수정 판매상품관리 기본상품 get 완료

--- static/js/setting.js
+++ static/js/setting.js
@@ -457,7 +457,7 @@ function put_schedule(data){
             if (head.code === 401) {
                 pop.open('firstRequestMsg1', '잠시 후 다시 시도 해주세요.');
             } else if (head.code === 200) {
-                pop.open('firstRequestMsg1', '완료되었습니다.');
+                pop.open('scheduleBackUrl', '완료되었습니다.');
             }
         }
     })
@@ -581,4 +581,275 @@ function get_etc_product(id){
             }
         }
     })
+}
+
+// 메인상품 뿌려주기
+function view_beauty_product(){
+    // 미용별 소요시간
+    var col_html = '<colgroup>';
+    var thead_html = '<thead><tr>';
+    var tbody_html = '<tbody><tr>';
+    var add_service = ['무게'];
+    $.each(setting_array[0].worktime, function(i,v){
+        var txt = '';
+        switch (i){
+            case 'bath' : txt = '목욕'; break;
+            case 'part' : txt = '부분미용'; break;
+            case 'bath_part' : txt = '부분+목욕'; break;
+            case 'sanitation' : txt = '위생'; break;
+            case 'sanitation_bath' : txt = '위생+목욕'; break;
+            case 'all' : txt = '전체미용'; break;
+            case 'spoting' : txt = '스포팅'; break;
+            case 'scissors' : txt = '가위컷'; break;
+            case 'summercut' : txt = '썸머컷'; break;
+            default : txt = i; add_service.push(txt);
+        }
+        col_html += '<col style="width:auto;">';
+        thead_html += `<th>${txt}</th>`;
+        tbody_html += `<td>${v.time}</td>`;
+    })
+    col_html += '</colgroup>';
+    thead_html += '</tr></thead>';
+    tbody_html += '</tr></tbody>';
+    $(".beauty_time_wrap").html(col_html+thead_html+tbody_html);
+
+    // 강아지 미용 가격
+    //console.log(setting_array[0].dog);
+    var dog_total_html = '';
+    $.each(setting_array[0].dog, function(index,value){
+        var service_type = '';
+        if(value.in_shop == 1 && value.out_shop == 1){
+            service_type = '출장/매장';
+        }else if(value.in_shop == 1){
+            service_type = '매장상품';
+        }else if(value.out_shop == 1){
+            service_type = '출장상품';
+        }
+        var dog_title = '';
+        if($.trim(value.second_type) == '직접입력' && value.direct_title != ''){
+            dog_title = value.direct_title;
+        }else{
+            dog_title = value.second_type;
+        }
+        var dog_st_html = `
+            <div class="basic-data-group">
+                <div class="con-title-group large">
+                    <h6 class="con-title">${dog_title}<div class="label label-outline-purple vmiddle round">${service_type}</div></h6>
+                </div>
+                <div class="read-table">
+                    <div class="read-table-unit large">(단위:원)</div>
+                    <table>
+        `;
+        var dog_col_html = `<colgroup>`;
+        var dog_thead_html = `<thead><tr>`;
+        var dog_tbody_html = ``;
+
+        var is_service = [];
+        $.each(value.service, function(i,v){
+            dog_tbody_html += `<tbody><tr>`;
+            var add_number = 0;
+            var empty_td = 0;
+            $.each(v, function(_i, _v){
+                //console.log(i);
+                var txt = '';
+                switch (_i){
+                    case 'bath_price' : txt = '목욕'; break;
+                    case 'part_price' : txt = '부분미용'; break;
+                    case 'bath_part_price' : txt = '부분+목욕'; break;
+                    case 'sanitation_price' : txt = '위생'; break;
+                    case 'sanitation_bath_price' : txt = '위생+목욕'; break;
+                    case 'all_price' : txt = '전체미용'; break;
+                    case 'spoting_price' : txt = '스포팅'; break;
+                    case 'scissors_price' : txt = '가위컷'; break;
+                    case 'summercut_price' : txt = '썸머컷'; break;
+                    default : txt = add_service[add_number]; add_number ++;
+                }
+
+                if(is_service.indexOf(_i) < 0){
+                    dog_col_html += `<col style="width:auto;">`;
+                    dog_thead_html += `<th>${txt}</th>`;
+                    is_service.push(_i);
+                }
+
+
+                if(_i == 'kg'){
+                    dog_tbody_html += `<td>~ ${_v}kg</td>`;
+                }else{
+                    //console.log(_v.consult);
+                    var price = '';
+                    if(_v.consult == 0){
+                        price = (_v.price).format();
+                    }else{
+                        price = '상담';
+                    }
+                    if(is_service.indexOf(_i) == empty_td){
+                        dog_tbody_html += `<td>${price}</td>`;
+                    }else{
+                        var cnt = is_service.indexOf(_i) - empty_td;
+                        for(var i=0;i<cnt;i++){
+                            dog_tbody_html += `<td></td>`;
+                            empty_td ++;
+                        }
+                        dog_tbody_html += `<td>${price}</td>`;
+                    }
+
+                }
+                empty_td ++;
+            })
+            dog_tbody_html += `</tr></tbody>`;
+        })
+
+        dog_col_html += `</colgroup>`;
+        dog_thead_html += `</tr></thead>`;
+
+        if(value.is_over_kgs == 1){
+            var colspan = is_service.length - 1;
+            dog_tbody_html += `
+                    <tr>
+                        <td>${value.over_kg}kg ~</td>
+                        <td colspan="${colspan}">kg당 ${(value.over_price).format()}원 추가</td>
+                    </tr>
+                `;
+        }
+
+        var dog_fi_html = `
+                    </table>
+                </div>
+            <div class="basic-data-group large">
+                <div class="memo-item large">
+                    <div class="memo-item-title">상품별 안내사항</div>
+                    <div class="memo-item-txt">${db_to_str(value.comment)}</div>
+                </div>
+            </div>
+            <div class="btn-basic-action">
+                <div class="grid-layout btn-grid-group">
+                    <div class="grid-layout-inner justify-content-end">
+                        <div class="grid-layout-cell flex-auto"><button type="button" class="btn btn-outline-purple btn-small-size btn-basic-small">수정</button></div>
+                        <div class="grid-layout-cell flex-auto"><button type="button" class="btn btn-outline-gray btn-small-size btn-basic-small">삭제</button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        dog_total_html += dog_st_html + dog_col_html + dog_thead_html + dog_tbody_html + dog_fi_html;
+    })
+    $(".append_dog_service_wrap").append(dog_total_html);
+
+    // 공통 안내사항
+    $(".etc_comment").text(setting_array[0].etc_comment);
+
+    // 부가세
+    if(setting_array[0].is_vat == 0){
+        $("#switch-toggle").prop("checked", false);
+    }else{
+        $("#switch-toggle").prop("checked", true);
+    }
+
+    // 고양이 미용가격
+    //console.log(setting_array[0].cat);
+    if(setting_array[0].cat != ''){
+        var cat_product_type = '';
+        if(setting_array[0].cat.in_shop == 1 && setting_array[0].cat.out_shop == 1){
+            cat_product_type = '출장/매장';
+        }else if(setting_array[0].cat.in_shop == 1){
+            cat_product_type = '매장상품';
+        }else if(setting_array[0].cat.out_shop == 1){
+            cat_product_type = '출장상품';
+        }
+        $(".cat_product_type").text(cat_product_type);
+
+        var cat_col_html = `<colgroup>`;
+        var cat_thead_html = `<thead><tr>`;
+        var cat_tbody_html = `<tbody>`;
+        if(setting_array[0].cat.is_use_weight == 1){
+            cat_col_html += `
+                    <col style="width:16.66%;">
+                    <col style="width:auto;">
+                    <col style="width:auto;">
+                </colgroup>
+            `;
+            cat_thead_html += `
+                        <th>무게</th>
+                        <th>단모</th>
+                        <th>장모</th>
+                    </tr>
+                </thead>
+            `;
+            var section = (setting_array[0].cat.section).split(',');
+            for(var i=0;i<section.length;i++){
+                console.log(section[i]);
+                cat_tbody_html += `
+                    <tr>
+                        <td>~ ${section[i]}kg</td>
+                        <td>${(setting_array[0].cat.service[i].short_price).format()}</td>
+                        <td>${(setting_array[0].cat.service[i].long_price).format()}</td>
+                    </tr>
+                `;
+            }
+            cat_tbody_html += `</tbody>`;
+        }else{
+            cat_col_html += `
+                    <col style="width:auto;">
+                    <col style="width:auto;">
+                </colgroup>
+            `;
+            cat_thead_html += `
+                        <th>단모</th>
+                        <th>장모</th>
+                    </tr>
+                </thead>
+            `;
+            cat_tbody_html += `
+                <tr>
+                    <td>${(setting_array[0].cat.short_price).format()}</td>
+                    <td>${(setting_array[0].cat.long_price).format()}</td>
+                </tr>
+            `;
+        }
+        $(".cat_beauty_price_wrap").html(cat_col_html+cat_thead_html+cat_tbody_html);
+
+        $(".shower_price").text((setting_array[0].cat.shower_price).format());
+        $(".shower_price_long").text((setting_array[0].cat.shower_price_long).format());
+        if(setting_array[0].cat.shower_price_long == '' && setting_array[0].cat.shower_price == ''){
+            $(".shower_wrap").css("display","none");
+        }
+
+        $(".hair_clot_price").text((setting_array[0].cat.hair_clot_price).format());
+        if(setting_array[0].cat.hair_clot_price == ''){
+            $(".hair_clot_wrap").css("display","none");
+        }
+        $(".ferocity_price").text((setting_array[0].cat.ferocity_price).format());
+        if(setting_array[0].cat.ferocity_price == ''){
+            $(".ferocity_wrap").css("display","none");
+        }
+        var cat_shop_option_html = ``;
+        $.each(setting_array[0].cat.shop_option, function(i,v){
+            cat_shop_option_html += `
+                <tr>
+                    <td>${i}</td>
+                    <td>${v.format()}</td>
+                </tr>
+            `;
+        })
+        $(".shop_option_append_wrap").append(cat_shop_option_html);
+
+        $(".toenail_price").text((setting_array[0].cat.toenail_price).format());
+        if(setting_array[0].cat.toenail_price == ''){
+            $(".toenail_wrap").css("display","none");
+        }
+        var cat_option_html = ``;
+        $.each(setting_array[0].cat.option, function(i,v){
+            cat_option_html += `
+                <tr>
+                    <td>${i}</td>
+                    <td>${v.format()}</td>
+                </tr>
+            `;
+        })
+        $(".option_append_wrap").append(cat_option_html);
+        $(".cat_comment").text(setting_array[0].cat.comment);
+
+        $(".total_cat_wrap").css("display","block");
+        $(".cat_none_wrap").css("display","none");
+    }
 }
